356fb028558996f7b7970db53d270d7ae6652e15
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-10-14 10:42:26 -0600

    Anotaciones en PrestamoDto

--- proyectoBiblioteca/src/main/java/com/biblioteca/dto/PrestamoDto.java
+++ proyectoBiblioteca/src/main/java/com/biblioteca/dto/PrestamoDto.java
@@ -3,6 +3,11 @@ package com.biblioteca.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
 
 
 public class PrestamoDto implements Serializable{
@@ -11,11 +16,23 @@ public class PrestamoDto implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	@NotNull(message = "El libro es obligatorio")
+	@Positive(message = "El Id del libro debe ser positivo")
 	private Long libroId;
+	
+	@NotNull(message = "El usuario es obligatorio")
+	@Positive(message = "El Id del usuario es obligatorio")
 	private Long usuarioId;
 	
+	@PastOrPresent(message = "La fecha de prestamo no puede ser futura")
 	private LocalDate fechaPrestamo;
+	
+	@FutureOrPresent(message = "La fecha de devolucion no puede ser en el pasado")
 	private LocalDate fechaDevolucion;
+	
+	@NotNull(message = "Debe indicar si el libro fue devuelto o no")
 	private Boolean devuelto;
 	
 	public Long getLibroId() {
