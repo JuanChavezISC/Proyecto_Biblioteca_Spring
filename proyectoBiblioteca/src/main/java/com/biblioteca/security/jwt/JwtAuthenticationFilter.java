e2a6ba83fa921778604e9de38a75fd975b09a2b8
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-11-21 12:01:52 -0600

    Creacion de clases JWT

---
+++ proyectoBiblioteca/src/main/java/com/biblioteca/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.biblioteca.security.jwt;
+
+import com.biblioteca.security.service.JpaUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component @RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final JpaUserDetailsService userDetailsService;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        System.out.println("PATH FILTRADO: " + request.getServletPath());
+        return request.getServletPath().startsWith("/api/auth/");
+
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")){
+            String token = authHeader.substring(7);
+            try {
+                String username = jwtService.extractUsername(token);
+                var userDetails = userDetailsService.loadUserByUsername(username);
+                if (jwtService.isValid(token, userDetails.getUsername())){
+                    var auth = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }catch (Exception ignored){}
+        }
+        chain.doFilter(request, response);
+
+    }
+}
