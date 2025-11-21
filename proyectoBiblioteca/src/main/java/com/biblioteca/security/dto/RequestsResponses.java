37ff7c1927ea5f229d2039367e28f2b85775682c
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-11-21 11:52:05 -0600

    Creacion de Servicios y RequestsResponses

---
+++ proyectoBiblioteca/src/main/java/com/biblioteca/security/dto/RequestsResponses.java
@@ -0,0 +1,23 @@
+package com.biblioteca.security.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class RequestsResponses {
+    public record RegisterRequest(
+            @NotBlank String username,
+            @NotBlank @Email String email,
+            @NotBlank String password
+    ){}
+
+    public record LoginRequest(
+            @NotBlank String username,
+            @NotBlank String password
+    ){}
+
+    public record AuthResponse(
+            String token,
+            String tokenType,
+            long expiresInSeconds
+    ) {}
+}
