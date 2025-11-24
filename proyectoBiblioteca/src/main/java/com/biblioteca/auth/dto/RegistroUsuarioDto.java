bcac7dfb545b2324df6f0e07b82885d9ac2a5a28
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-11-24 14:02:29 -0600

    Creacion de Dto RegistroUsuario para registro de usuario

---
+++ proyectoBiblioteca/src/main/java/com/biblioteca/auth/dto/RegistroUsuarioDto.java
@@ -0,0 +1,33 @@
+package com.biblioteca.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegistroUsuarioDto(
+        @NotBlank
+                (message = "El nombre es obligatorio")
+        String nombre,
+
+        @NotBlank(message = "El apellido es obligatorio")
+        String apellido,
+
+        @NotBlank(message = "El email es obligatorio")
+        @Email(message = "Formato de email no válido")
+        String email,
+
+        @Size(min = 10, max = 15, message = "El teléfono debe tener entre 10 y 15 caracteres")
+        String telefono,
+
+        @NotBlank(message = "La dirección es obligatoria")
+        String direccion,
+
+        @NotBlank(message = "El username es obligatorio")
+        @Size(min = 4, max = 20, message = "El username debe tener entre 4 y 20 caracteres")
+        String username,
+
+        @NotBlank(message = "La contraseña es obligatoria")
+        @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+        String password
+) {
+}
